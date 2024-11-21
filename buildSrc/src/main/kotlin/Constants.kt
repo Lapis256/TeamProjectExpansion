f3b6a68e40828399df9fbf952597add2b708aec5
Lapis <45414333+Lapis256@users.noreply.github.com>
2024-11-22 05:37:11 +0900

    Bump version

--- buildSrc/src/main/kotlin/Constants.kt
+++ buildSrc/src/main/kotlin/Constants.kt
@@ -1,10 +1,10 @@
 object Constants {
     object Mod {
         const val id = "team_project_expansion"
-        const val name = "TeamProjectExpansion"
-        const val description = "TeamProjectExpansion"
+        const val name = "Team Project Expansion"
+        const val description = "Makes Project Expansion compatible with Team ProjectE, enabling offline use."
         const val license = "MIT"
-        const val version = "1.0.0"
+        const val version = "1.1.0"
         const val group = "io.github.lapis256"
         const val author = "Lapis256"
         const val repositoryUrl = "https://github.com/Lapis256/TeamProjectExpansion"
